f35fd829e716e4c8c9a803831e349a00e5674cad
apariani3 <apariani3@gatech.edu>
2022-04-08 03:03:26 -0400

    instructions change

--- src/Game.elm
+++ src/Game.elm
@@ -453,8 +453,8 @@ viewWelcomeMessage model =
     div [] [ div [ Attr.class "system-message" ] [ text """
     Welcome! Codenames Green is a cooperative word game. Players divide into
     two sides. Each side has nine green words that they must provide clues for.
-    Sides take turns giving one-word clues, plus a number indicating the
-    number of words it applies to. Then the other side guesses until they tap
+    Sides take turns giving one-word clues, and the target words on the board 
+    that it applies to according to you. Then the other side guesses until they tap
     a non-green word or choose to stop. Tapping a black instantly loses the game.
     Try to reveal all green words before the timer counter reaches 9.
     Good luck, have fun!
